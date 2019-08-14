66e9c745e6152cdca7bf9b74ae78a5b3d9176a6a
ButchStein <butchbs1982@gmail.com>
2019-08-14 14:06:17 +0300

    'a'

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+    <v-app>
+      <v-navigation-drawer app temporary>
+        <!-- -->
+      </v-navigation-drawer>
+
+      <v-app-bar app dark color="primary">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Первый SPA</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+          <v-btn text>Link 1</v-btn>
+          <v-btn text>Link 2</v-btn>
+          <v-btn text>Link 3</v-btn>
+        </v-toolbar-items>
+        <v-btn icon>
+          <v-icon>mdi-announcement</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-menu
+          left
+          bottom
+        >
+          <template v-slot:activator="{ on }">
+            <v-btn icon v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item
+              v-for="n in 5"
+              :key="n"
+              @click="() => {}"
+            >
+              <v-list-item-title>Option {{ n }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-app-bar>
+
+      <!-- Sizes your content based upon application components -->
+      <v-content>
+
+        <!-- Provides the application the proper gutter -->
+
+          <!-- If using vue-router -->
+          <router-view></router-view>
+
+      </v-content>
+
+      <v-footer app>
+        <!-- -->
+      </v-footer>
+    </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  })
+}
+</script>
